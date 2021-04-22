17db55d93b8de2ef61843ba598b015e13c790f0d
parkgun6 <76540460+parkgun6@users.noreply.github.com>
2021-04-22 12:34:09 +0900

    댓글 불러오기

--- board/src/main/java/org/zerock/board/repository/ReplyRepository.java
+++ board/src/main/java/org/zerock/board/repository/ReplyRepository.java
@@ -5,9 +5,14 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.zerock.board.entity.Reply;
 
+import java.util.List;
+
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
 
     @Modifying
     @Query("delete from Reply r where r.board.bno = :bno")
     void deleteByBno(Long bno);
+
+    @Query("select r from Reply r where r.board.bno = :bno order by r.rno asc ")
+    List<Reply> getBoardReply(Long bno);
 }
